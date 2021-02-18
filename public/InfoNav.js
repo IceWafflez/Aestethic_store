46c74096890f671fc145b4e12830cb21af34afde
IceWafflez <55033457+IceWafflez@users.noreply.github.com>
2021-02-18 17:27:11 +0100

    lagde nye funskjoner på header og store, samt bugfixer

---
+++ public/InfoNav.js
@@ -0,0 +1,32 @@
+
+  function openNavJørgen() {
+    document.getElementById("JørgenNav").style.height = "100%";
+  }
+  
+  function closeNavJørgen() {
+    document.getElementById("JørgenNav").style.height = "0%";
+  }
+
+  function openNavJohn() {
+    document.getElementById("JohnNav").style.height = "100%";
+  }
+  
+  function closeNavJohn() {
+    document.getElementById("JohnNav").style.height = "0%";
+  }
+
+  function openNavMing() {
+    document.getElementById("MingNav").style.height = "100%";
+  }
+  
+  function closeNavMing() {
+    document.getElementById("MingNav").style.height = "0%";
+  }
+
+  function openNavThea() {
+    document.getElementById("TheaNav").style.height = "100%";
+  }
+  
+  function closeNavThea() {
+    document.getElementById("TheaNav").style.height = "0%";
+  }
